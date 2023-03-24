a1b3a7f0718292d84105eb337a783670554184af
fjcanofuentes <francisco.canofuentes@gmail.com>
2023-03-24 19:29:42 +0100

    confetti() {   git add .   git commit -m "$1"   git push origin main }

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,30 @@
+import { useState } from 'react';
+import './Button.css';
+import confetti from "canvas-confetti";
+
 export default function Button({ children }) {
+
+  const [name, setName] = useState('');;
+
+  const handleChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleClick = (event) => {
+    alert(`Me la comes 1-0 ${name} 😛`);
+    confetti();
+  };
+
   return (
     <>
-      <h1>Hola Rob =)</h1>
-      <button onClick={() => alert("Me la comes 1-0")}>
+      <h1>Hola 😄</h1>
+      <div>
+        <label>Introduce tu nombre:</label>
+      </div>
+      <div>
+        <input type="text" placeholder='Perico' onChange={handleChange}/>
+      </div>
+      <button onClick={handleClick} className="button">
         {children}
       </button>
     </>
